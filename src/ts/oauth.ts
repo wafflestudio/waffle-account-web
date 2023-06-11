cbe6653d2640311a613d017ef424c1ce23676bcc
ars-ki-00 <dkwanm1@snu.ac.kr>
2023-06-11 14:52:38 +0900

    redirect uri 및 api path 버그 수정

--- src/ts/oauth.ts
+++ src/ts/oauth.ts
@@ -1,6 +1,32 @@
 import { AuthApi } from "../apis";
 import { SigninResponse } from "../types/Auth";
 
+const onSuccess = (res: SigninResponse) => {
+  try {
+    window.Android.onSuccessSignIn(res.access_token);
+    return;
+  } catch {}
+
+  try {
+    window.webkit.messageHandlers.onSuccessSignIn.postMessage(res.access_token);
+    return;
+  } catch {}
+
+  const cookieName = encodeURIComponent("waffle.access-token");
+  const cookiveValue = encodeURIComponent(res.access_token);
+  const domain = "wafflestudio.com";
+
+  document.cookie = `${cookieName}=${cookiveValue}; path=/; domain=${domain} `;
+
+  const redirectUri = localStorage.getItem("redirectUri");
+
+  if (redirectUri) {
+    window.location.href = redirectUri;
+
+    localStorage.removeItem("redriectUri");
+  }
+};
+
 window.onload = function () {
   if (window.location.href.includes("kakao")) {
     const params = new URLSearchParams(window.location.search);
@@ -11,24 +37,7 @@ window.onload = function () {
 
     AuthApi.oauthSignupWithCode("kakao", code)
       .then((res) => res.json())
-      .then((res: SigninResponse) => {
-        alert(res.access_token);
-        try {
-          window.Android.onSuccessSignIn(res.access_token);
-          return;
-        } catch {}
-
-        try {
-          window.webkit.messageHandlers.onSuccessSignIn.postMessage(
-            res.access_token
-          );
-          return;
-        } catch {}
-      })
-      .catch(() => {
-        alert("fail");
-      });
-
+      .then(onSuccess);
     return;
   } else if (window.location.href.includes("naver")) {
     const params = new URLSearchParams(window.location.search);
@@ -40,23 +49,7 @@ window.onload = function () {
 
     AuthApi.oauthSignupWithCode("naver", code, state)
       .then((res) => res.json())
-      .then((res: SigninResponse) => {
-        alert(res.access_token);
-        try {
-          window.Android.onSuccessSignIn(res.access_token);
-          return;
-        } catch {}
-
-        try {
-          window.webkit.messageHandlers.onSuccessSignIn.postMessage(
-            res.access_token
-          );
-          return;
-        } catch {}
-      })
-      .catch(() => {
-        alert("fail");
-      });
+      .then(onSuccess);
 
     return;
   } else if (window.location.href.includes("github")) {
@@ -68,23 +61,7 @@ window.onload = function () {
 
     AuthApi.oauthSignupWithCode("github", code)
       .then((res) => res.json())
-      .then((res: SigninResponse) => {
-        alert(res.access_token);
-        try {
-          window.Android.onSuccessSignIn(res.access_token);
-          return;
-        } catch {}
-
-        try {
-          window.webkit.messageHandlers.onSuccessSignIn.postMessage(
-            res.access_token
-          );
-          return;
-        } catch {}
-      })
-      .catch(() => {
-        alert("fail");
-      });
+      .then(onSuccess);
 
     return;
   } else if (window.location.href.includes("apple")) {
@@ -97,7 +74,6 @@ window.onload = function () {
     AuthApi.oauthSignupWithCode("apple", code)
       .then((res) => res.json())
       .then((res: SigninResponse) => {
-        alert(res.access_token);
         try {
           window.Android.onSuccessSignIn(res.access_token);
           return;
@@ -109,9 +85,6 @@ window.onload = function () {
           );
           return;
         } catch {}
-      })
-      .catch(() => {
-        alert("fail");
       });
 
     return;
@@ -125,20 +98,5 @@ window.onload = function () {
 
   AuthApi.oauthSignupWithCode("google", code)
     .then((res) => res.json())
-    .then((res: SigninResponse) => {
-      localStorage.setItem("accessToken", res.access_token);
-      localStorage.setItem("refreshToken", res.refresh_token);
-      try {
-        window.Android.onSuccessSignIn(res.access_token);
-        return;
-      } catch {}
-
-      try {
-        window.webkit.messageHandlers.onSuccessSignIn.postMessage(
-          res.access_token
-        );
-        return;
-      } catch {}
-    })
-    .catch();
+    .then(onSuccess);
 };
