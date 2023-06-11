5262493cc7416857a389a7e54f181a7dc9c4e38e
ars-ki-00 <dkwanm1@snu.ac.kr>
2023-06-11 16:05:12 +0900

    로컬 로그인 추가

--- src/ts/signin.ts
+++ src/ts/signin.ts
@@ -1,12 +1,39 @@
 import { AuthApi } from "../apis";
 import { SigninResponse } from "../types/Auth";
 
-const signinForm = document.getElementById("signin-form");
+const loginButton = document.getElementById("login-button");
 const emailInput = document.getElementById("email") as HTMLInputElement;
 const passwordInput = document.getElementById("password") as HTMLInputElement;
 
-signinForm?.addEventListener("submit", (e: SubmitEvent) => {
+const onSuccess = (res: SigninResponse) => {
+  try {
+    window.Android.onSuccessSignIn(res.access_token);
+    return;
+  } catch {}
+
+  try {
+    window.webkit.messageHandlers.onSuccessSignIn.postMessage(res.access_token);
+    return;
+  } catch {}
+
+  const cookieName = encodeURIComponent("waffle.access-token");
+  const cookiveValue = encodeURIComponent(res.access_token);
+  const domain = "wafflestudio.com";
+
+  document.cookie = `${cookieName}=${cookiveValue}; path=/; domain=${domain} `;
+
+  const redirectUri = localStorage.getItem("redirectUri");
+
+  if (redirectUri) {
+    window.location.href = redirectUri;
+
+    localStorage.removeItem("redriectUri");
+  }
+};
+
+loginButton?.addEventListener("click", (e) => {
   e.preventDefault();
+  e.stopPropagation();
 
   if (!emailInput.value || !passwordInput.value) {
     alert("이메일과 비밀번호를 입력해 주세요.");
@@ -18,10 +45,6 @@ signinForm?.addEventListener("submit", (e: SubmitEvent) => {
     password: passwordInput.value,
   })
     .then((res) => res.json())
-    .then((res: SigninResponse) => {
-      localStorage.setItem("accessToken", res.access_token);
-      localStorage.setItem("refreshToken", res.refresh_token);
-      window.location.href = "/index.html";
-    })
+    .then(onSuccess)
     .catch(() => alert("이메일과 비밀번호를 확인해 주세요."));
 });
